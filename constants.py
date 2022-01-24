1d9762d15c7d01ba9963759bf2210a39039b2fdc
Davidogola <97657552+Davidogola@users.noreply.github.com>
2022-01-24 09:58:48 +0300

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,11 @@
+x=10
+print(x)
+print(type(x))
+c="hello programmer"
+print(c)
+print(type(c))
+b=40.78456
+print(b)
+print(type(b))
+d=3+2j
+print(type(d)
---
+++ type.py
@@ -0,0 +1,5 @@
+h=int(input("enter hour: "))
+r=float(input("enter rate: "))
+p=float(input("pay:"))
+grossPay=h*r*p
+print(grossPay)
