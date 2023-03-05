85260de5c7e3b03a7b998d261c2e84f668c6fd4a
Davidogola <97657552+Davidogola@users.noreply.github.com>
2023-03-05 21:48:55 +0300

    Create userinput.py

---
+++ userinput.py
@@ -0,0 +1,10 @@
+print("create your account now")
+user=input("enter username: ")
+pwd=input("enter password: ")
+print("your account was created successfully. login now")
+user1=input("enter your username: ")
+pwd1=input("enter your password: ")
+if user==user1 and pwd==pwd1:
+  print("logged in succesfull")
+elae:
+  print("invalid credential")
